536c756df52bf53da0e0842c0a45b209ca85767c
JulianNicolai <juliannicolai123@gmail.com>
2024-02-09 01:34:31 -0500

    Added new Python wrapper test using Cython.

---
+++ Software/Backend/PythonWrapperTest2/func.pyx
@@ -0,0 +1,7 @@
+def text_func(int a, int b):
+
+    cdef int constant
+
+    constant = 15
+
+    return a * b + constant
---
+++ Software/Backend/PythonWrapperTest2/wrapper.py
@@ -0,0 +1,7 @@
+import func
+
+a, b = 3, 7
+
+result = func.text_func(a, b)
+
+print(result)
