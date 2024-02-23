252fef27ac527070faec638d32576fd618f0989e
JulianNicolai <juliannicolai123@gmail.com>
2024-02-22 20:47:56 -0500

    Modified ADC script w/ data queue and improvements

--- Software/Backend/ProcessADC/main.py
+++ Software/Backend/ProcessADC/main.py
@@ -1,18 +1,20 @@
 from time import sleep
-from multiprocessing import Event
+from multiprocessing import Event, Queue
 
 from CaptureLightIntensity import CaptureLightIntensity
 
 shutdown_event = Event()
 shutdown_event.clear()
 
-capture_process = CaptureLightIntensity(shutdown_event, 27, 5)
+data_queue = Queue()
+
+capture_process = CaptureLightIntensity(shutdown_event, data_queue, 27, 5)
 capture_process.start()
 
 i = 0
 while i < 30:
-    print("Waiting around...")
-    sleep(1)
+    data = data_queue.get(block=True)
+    print(f"S0: {data[0]}, S1: {data[1]}, S2: {data[2]}, S3: {data[3]}")
     i += 1
 
 shutdown_event.set()
